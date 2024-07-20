6b27f0d41e24f2af49c0954996a7fa8e8cc600b0
Keenan Graham <ksgraham@stanford.edu>
2024-07-20 13:52:35 -0700

    Update feeds

--- willitsurf/feed.py
+++ willitsurf/feed.py
@@ -1,6 +1,8 @@
 urls = {
     'nob': 'https://cams.cdn-surfline.com/cdn-wc/wc-oceanbeachov/playlist.m3u8',
     'sob': 'https://cams.cdn-surfline.com/cdn-wc/wc-vicente/playlist.m3u8',
-    'nlm': 'https://cams.cdn-surfline.com/cdn-wc/wc-pacificaov/playlist.m3u8',
-    'slm': 'https://cams.cdn-surfline.com/cdn-wc/wc-pacificalindamar/playlist.m3u8',
+    'nlm': 'https://cams.cdn-surfline.com/cdn-wc/wc-northendpacifica/playlist.m3u8',
+    'slm': 'https://cams.cdn-surfline.com/cdn-wc/wc-boatdockspacifica/playlist.m3u8',
+    'tblm': 'https://cams.cdn-surfline.com/cdn-wc/wc-tacobellpacifica/playlist.m3u8',
+    'tbclm': 'https://cams.cdn-surfline.com/cdn-wc/wc-creekpacifica/playlist.m3u8',
 }
