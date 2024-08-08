8600af2042712214297b30d59dce673613aa0fc1
Keenan Graham <ksgraham@stanford.edu>
2024-08-08 15:53:34 -0700

    Remove comments

--- willitsurf/models/conv.py
+++ willitsurf/models/conv.py
@@ -118,15 +118,6 @@ def test(model, device, test_dataloader):
     )
 
 
-'''
-import torch; import numpy as np; import PIL; from willitsurf.models.conv import ConvNet
-x = torch.from_numpy(np.array(PIL.Image.open('./assets/data/raw/slm-2024-07-09-15-36-31-image-2.png')).transpose(2, 0, 1))
-d = (x / 255.0).unsqueeze(0).repeat(5, 1, 1, 1)
-c = ConvNet()
-c.forward(x)
-'''
-
-# Normalize train_X.mean() / 256.0, train_x.std() / 256.0
 def make_train_dataloader(train_data):
     return torch.utils.data.DataLoader(
         test_data,
@@ -141,28 +132,3 @@ def make_test_dataloader(test_data):
         batch_size=32,
         shuffle=True
     )
-
-# torch.manual_seed(0)
-# device = torce.device('cpu')
-# model = ConvNet()
-# optimizer = optim.Adadelta(model.parameters(), lr=0.5)
-
-#for epoch in range(1, 3):
-#    train(model, device, train_dataloader, optimizer, epoch)
-#    trest(model, device, test_dataloader)
-
-'''
-from torch.utils.data import DataLoader
-from torch.optim import Adadelta
-from willitsurf.models.conv import ConvNet
-from willitsurf.dataset import SurfImageDataset
-from willitsurf.models.conv import train
-s = SurfImageDataset('./assets/data/labels/annotations.tsv', './assets/data/raw')
-d = DataLoader(s, batch_size=10, shuffle=True)
-c = ConvNet()
-c.to('mps')
-a = Adadelta(c.parameters(), lr=0.5)
-train(c, 'cpu', d, a, 1)
-from willitsurf.models.conv import test
-test(c, 'cpu', d)
-'''
